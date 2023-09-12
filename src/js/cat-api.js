e197fb0cd6ea187308d959f133d60b1d2bdb0a1f
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-09-12 08:40:21 +0300

    змінив написання експорту, та створив змінну BASE_URL

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,22 +3,24 @@ import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] = 'live_d6fiC213Ac2c5lUISBnpZeN7aC5ou80plZ9P6kmgj980h1PBxMUjDazZTTgi3ETu';
 // отримуємо список порід котів з API
-async function fetchBreeds() {
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+export async function fetchBreeds() {
   try {
-    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+    const response = await axios.get(`${BASE_URL}/breeds`);
     return response.data;
   } catch (error) {
     throw error;
   }
 }
 // отримуємо інформацію про кота за ID
-async function fetchCatByBreed(breedId) {
+export async function fetchCatByBreed(breedId) {
   try {
-    const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
+    const response = await axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`);
     return response.data;
   } catch (error) {
     throw error;
   }
 }
 
-export { fetchBreeds, fetchCatByBreed };
